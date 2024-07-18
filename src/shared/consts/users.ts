322db9c91b8a8d157bdaad848e3da889f68d82ac
CERF <limbooo689@gmail.com>
2024-07-18 10:31:41 +0300

    fix #3.1

--- src/shared/consts/users.ts
+++ src/shared/consts/users.ts
@@ -4,7 +4,7 @@ export const USER_LIST: IUser[] = [
     {
         id: '1',
         nickname: 'Jav',
-        avatar: '', //https://s3-alpha-sig.figma.com/img/c05d/1b14/f3a120be540c5f09aa4efa717c427906?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cpeKvkxrcbggOMQHDQ2xQE3oeSCh6fd3zZuNuXjqkVTF8b5QbBgp796O3YEwxHiAn7P57aNVA9Ln3VF43RfBgEcL-NyJUqaULHAsIyjOuxVUL7jJFFVA9O-4G5qL7a0yhx8UQeKJfY0jKWt5Hph1BsJFAUEdF4E8PMBX~yP42Ekx0nCbObNdfanN3L1x~Y35GQ0NlAoC1lxDpRwxmh-AsgQALMl2cd9ejo0UviHbhJxdehsXi2ERypZmWsNGKWtijVBCrF0-XOi2qGz~ZACSqIgQBX9rzzCpL2PP1zZFFgPunGhlFPI7Bcw6NcGtNvT2-Q2ZmEA-vnYYpEm7HGVdaA__
+        avatar: 'https://s3-alpha-sig.figma.com/img/c05d/1b14/f3a120be540c5f09aa4efa717c427906?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cpeKvkxrcbggOMQHDQ2xQE3oeSCh6fd3zZuNuXjqkVTF8b5QbBgp796O3YEwxHiAn7P57aNVA9Ln3VF43RfBgEcL-NyJUqaULHAsIyjOuxVUL7jJFFVA9O-4G5qL7a0yhx8UQeKJfY0jKWt5Hph1BsJFAUEdF4E8PMBX~yP42Ekx0nCbObNdfanN3L1x~Y35GQ0NlAoC1lxDpRwxmh-AsgQALMl2cd9ejo0UviHbhJxdehsXi2ERypZmWsNGKWtijVBCrF0-XOi2qGz~ZACSqIgQBX9rzzCpL2PP1zZFFgPunGhlFPI7Bcw6NcGtNvT2-Q2ZmEA-vnYYpEm7HGVdaA__',
         online: true,
         vacancy: 'QA Engineer',
         lastSeen: new Date('2024-07-15T12:20:31.999Z'),
@@ -12,7 +12,7 @@ export const USER_LIST: IUser[] = [
     {
         id: '2',
         nickname: 'Aubrey',
-        avatar: '', //https://s3-alpha-sig.figma.com/img/1c10/3a1a/cc0b9825064a996efd2dc58920faabd3?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pZ2liR9NFxbfAGBsJ2NtVX3nyqGZ7iXL8RPH88I66N7uc9dLYsnASY~EzDc-sqNDTP3dpGfbvIEWwssxQMCRbCjKyo~gEbsnfHEDhHOmjft6IWK9N1o4QcOVOD5XmdV6ZC35qM4luYyDA20MeSDW61sSt8xjkpm1IX~jS3sdGDcfak-nMaUz5ZFkOpcYbsvoEfse3oGypbN2b04dvubfirPl-oaQXPBLJIqoDEyS0ExdVCLkEUdSNcnLwAL-MWg519aja705eSZ289oj9dFtq6q8zbVv19o5kouLOX4oy3dmjNdKtkmD-y-UWMjRTrv3C88QVCR~PEac5i35B6uiQw__
+        avatar: 'https://s3-alpha-sig.figma.com/img/1c10/3a1a/cc0b9825064a996efd2dc58920faabd3?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pZ2liR9NFxbfAGBsJ2NtVX3nyqGZ7iXL8RPH88I66N7uc9dLYsnASY~EzDc-sqNDTP3dpGfbvIEWwssxQMCRbCjKyo~gEbsnfHEDhHOmjft6IWK9N1o4QcOVOD5XmdV6ZC35qM4luYyDA20MeSDW61sSt8xjkpm1IX~jS3sdGDcfak-nMaUz5ZFkOpcYbsvoEfse3oGypbN2b04dvubfirPl-oaQXPBLJIqoDEyS0ExdVCLkEUdSNcnLwAL-MWg519aja705eSZ289oj9dFtq6q8zbVv19o5kouLOX4oy3dmjNdKtkmD-y-UWMjRTrv3C88QVCR~PEac5i35B6uiQw__',
         online: false,
         vacancy: 'Product',
         lastSeen: new Date('2024-07-15T12:20:31.999Z'),
@@ -20,7 +20,7 @@ export const USER_LIST: IUser[] = [
     {
         id: '3',
         nickname: 'Janet',
-        avatar: '', //https://s3-alpha-sig.figma.com/img/10e8/ca44/0d13be8eb41c740c86723de252569da9?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=lz8EBus9RK~CuhRMM662blfH1SZj19SF2zGcIXMnzCPpY3qxt0CvdHlnH66WylLHlIJ6zMS-z4xj7x-Fsfl6K5t6Z6VJyV~QkEN4ZnyF83fcIy-qx-FAK5CVsdiMZ0FYJZ2wTeYVA2a39SBttAmbnP5m-ezXINd3jyA4JwZGmiDdMWFxBWcineFhxUT1ZysNlrAAfzEOQ0IFOBHCMX~ylJH3vB4OR674lU2zvxvbvw8WlSzbu~mmN18PpFc1GXuUhH4WcZ7Covw3cEiqL8hY6LG~-hoSUH3O86wkUWw3WhOocjLPrkAoC-ZuMs2od8EMKTdJHAhM4zcZAYzEZuGQjQ__
+        avatar: 'https://s3-alpha-sig.figma.com/img/10e8/ca44/0d13be8eb41c740c86723de252569da9?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=lz8EBus9RK~CuhRMM662blfH1SZj19SF2zGcIXMnzCPpY3qxt0CvdHlnH66WylLHlIJ6zMS-z4xj7x-Fsfl6K5t6Z6VJyV~QkEN4ZnyF83fcIy-qx-FAK5CVsdiMZ0FYJZ2wTeYVA2a39SBttAmbnP5m-ezXINd3jyA4JwZGmiDdMWFxBWcineFhxUT1ZysNlrAAfzEOQ0IFOBHCMX~ylJH3vB4OR674lU2zvxvbvw8WlSzbu~mmN18PpFc1GXuUhH4WcZ7Covw3cEiqL8hY6LG~-hoSUH3O86wkUWw3WhOocjLPrkAoC-ZuMs2od8EMKTdJHAhM4zcZAYzEZuGQjQ__',
         online: false,
         vacancy: 'Product',
         lastSeen: new Date('2024-07-15T12:20:31.999Z'),
@@ -28,7 +28,7 @@ export const USER_LIST: IUser[] = [
     {
         id: '4',
         nickname: 'Lidia',
-        avatar: '', //https://s3-alpha-sig.figma.com/img/dbaa/16a5/0e7db451d868b67382bd8b61d8ac3da1?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Vjd20QBKb8arPxximFP69Y9KbnrFpeuDIVcRh8hUlt8J62May120ne-l5gTfwqd6lUOnJ-Zp~8vpFyX2IKg-udjsLg9gNuZrBbYk2Vu~b8iYpBY8yOpua2L7sXmDbpsxpzl2cbvEqPES-17kP81TFcf4jOHMqXpB2W~zu7GgjGr30FucVoRPG~UV-JyQCeebZrdAK5zFyAEvBUoF18bsWXtl~px6IX-G5wjwx2G26ip29PqitqMth1aqD9OQBPsNI-eutymWL0jbPmB2EqhibYTpZjsg3~xCNx6xAy8ztKlMb~a4W3PX9-zxOhPFG60OLJgNgKlS6MoBsbdZc6liwA__
+        avatar: 'https://s3-alpha-sig.figma.com/img/dbaa/16a5/0e7db451d868b67382bd8b61d8ac3da1?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Vjd20QBKb8arPxximFP69Y9KbnrFpeuDIVcRh8hUlt8J62May120ne-l5gTfwqd6lUOnJ-Zp~8vpFyX2IKg-udjsLg9gNuZrBbYk2Vu~b8iYpBY8yOpua2L7sXmDbpsxpzl2cbvEqPES-17kP81TFcf4jOHMqXpB2W~zu7GgjGr30FucVoRPG~UV-JyQCeebZrdAK5zFyAEvBUoF18bsWXtl~px6IX-G5wjwx2G26ip29PqitqMth1aqD9OQBPsNI-eutymWL0jbPmB2EqhibYTpZjsg3~xCNx6xAy8ztKlMb~a4W3PX9-zxOhPFG60OLJgNgKlS6MoBsbdZc6liwA__',
         online: true,
         vacancy: 'Engineering',
         lastSeen: new Date('2024-07-15T12:20:31.999Z'),
