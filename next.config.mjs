e4eda68f536453faff1cbf8e974a64abbcbccb75
CERF <limbooo689@gmail.com>
2024-07-18 10:29:36 +0300

    fix #3

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,29 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.m?js$/,
+            resolve: {
+                fullySpecified: false, // disable the behavior
+            },
+            exclude: /node_modules/, // if you want to transpile some specific module
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['next/babel'],
+                },
+            },
+        });
+        return config;
+    },
 };
 
 export default nextConfig;
